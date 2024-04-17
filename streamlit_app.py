5d3f29bd7dd71a3ca540979d7ee9e2e85c46cf66
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-04-17 10:57:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+In the meantime, below is an example of what you cannnn do with just a few lines of code:
 """
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
